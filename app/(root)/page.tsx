1f5cf5462ab082e9f41073e9072c5416bdc217ff
Ayaan Siddiqui <ars394@pitt.edu>
2024-07-30 22:18:58 +0530

    responsive home page for mobile devices

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,18 +16,27 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">Turning Pixels into Wizardry</h1>
-        <ul className="flex-center w-full xl:gap-16 gap-10 flex-wrap lg:flex-nowrap">
+        <ul className="flex-center w-full xl:gap-20 md:gap-10 gap-4 flex-wrap lg:flex-nowrap mt-20">
           {navLinks.slice(1, 6).map((navLink) => (
             <Link
               key={navLink.route}
               href={navLink.route}
               className="flex-center flex-col gap-2"
             >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={navLink.icon} alt="icon" width={24} height={24} />
+              <li className="flex-center md:w-fit rounded-full bg-white p-2 md:p-3">
+                <Image
+                  src={navLink.icon}
+                  alt="icon"
+                  layout="responsive"
+                  objectFit="cover"
+                  height={24}
+                  width={24}
+                />
               </li>
-              <p className="p-14-medium text-center text-white">{navLink.label}</p>
-              <p className="text-xs font-semibold text-center italic mt-1 text-white">
+              <p className="font-medium md:text-[14px] text-[12px] md:leading-[120%] leading-[80%] mb-0 text-center text-white">
+                {navLink.label}
+              </p>
+              <p className="text-[12px] font-semibold text-center md:leading-[120%] leading-[80%] italic md:mt-1 mt-0 text-white">
                 {navLink.spell}
               </p>
             </Link>
